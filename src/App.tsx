6f34d7b4cf02734acbc86e35d8928b05b2becf35
andrei-brando <albertbrando@hotmail.com>
2021-12-29 08:47:11 -0300

    adiciona mirage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          category: 'food',
+          amount: 400,
+          type: 'deposit',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
